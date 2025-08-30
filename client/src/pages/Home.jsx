53d4895474f5da9cb95ff675ec1657678f449403
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-08-30 11:57:02 +0100

    create home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,24 @@
+
 import { lazy } from 'react'
 
 const Hero = lazy(() => import('../components/Hero'))
-const RadialOrbitalTimelineDemo = lazy(() => import('../components/Features'))
 const NavbarDemo = lazy(() => import('../components/NavbarSection'))
+const RuixenSection = lazy(() => import('../components/Features'))
+const LandingAccordionItem = lazy(() => import('../components/About'))
+const Testimonials = lazy(() => import('../components/Testimonial'))
+const Pricing = lazy(() => import('../components/Pricing'))
+const Footerdemo = lazy(() => import('../components/Footer'))
 
 const Home = () => {
   return (
     <div>
       <NavbarDemo />
       <Hero />
-      <RadialOrbitalTimelineDemo />
+      <LandingAccordionItem/>
+      <RuixenSection/>
+      <Testimonials/>
+      <Pricing/>
+      <Footerdemo/>
     </div>
   )
 }
