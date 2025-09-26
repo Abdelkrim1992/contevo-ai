d41b8730091ccd1f2affb482fe9493973ae4ad0a
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-09-26 10:53:00 +0100

    update server env

--- server/config/env.js
+++ server/config/env.js
@@ -10,4 +10,6 @@ export const ENV = {
     DATABASE_PASSWORD : process.env.DATABASE_PASSWORD,
     DATABASE_HOST : process.env.DATABASE_HOST,
     DATABASE_PORT : process.env.DATABASE_PORT,
+    JWT_SECRET : process.env.JWT_SECRET,
+    FRONTEND_APP_URL : process.env.FRONTEND_APP_URL,
 }
